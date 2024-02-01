091d50b5ab58640aac9d7178c8b3952d949d5dce
maximp7 <144476708+maximp7@users.noreply.github.com>
2024-02-01 01:00:16 -0500

    wer

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,17 @@
-import './style.css'
+import './style.css';  
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+document.getElementById('generateBtn').onclick = function() {
+  generateRandomNumber();
+};
+
+function generateRandomNumber() {
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  document.getElementById('generatedNumberDisplay').textContent = `Generated Number: ${randomNumber}`;
+
+  if (randomNumber > 5) {
+    console.log('The generated number is greater than 5.');
+  } else {
+    console.log('The generated number is 5 or less.');
+  }
+}
